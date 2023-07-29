5f56b2e7f7d5d3122e4c717804504fba0efce67d
arunkkumar1 <89664296+arunkkumar1@users.noreply.github.com>
2023-07-29 18:26:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
-# document summarization using huggingface transformers
 from transformers import pipeline
 import gradio as gr
 
-# load model
-model = pipeline('summarization')
 
-# define function
+model = pipeline(
+    "summarization",
+)
+
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-# launch interface
-with gr.Blocks() as demo:
-    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn=predict, inputs=textbox, outputs="text")
 
-# launch interface
-demo.launch()
-
+# create an interface for the model
+with gr.Interface(predict, "textbox", "text") as interface:
+    interface.launch()
